f6fbf9e401ebd480c173c6e5b4e8e123b1da8b73
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2020-03-10 13:14:26 -0400

    Initial Commit

---
+++ i2020_03_10_ReSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i2020_03_10_ReSwift
+//
+//  Created by Bradford, Phillip on 3/10/20.
+//  Copyright © 2020 Bradford, Phillip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ i2020_03_10_ReSwiftTests/i2020_03_10_ReSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  i2020_03_10_ReSwiftTests.swift
+//  i2020_03_10_ReSwiftTests
+//
+//  Created by Bradford, Phillip on 3/10/20.
+//  Copyright © 2020 Bradford, Phillip. All rights reserved.
+//
+
+import XCTest
+@testable import i2020_03_10_ReSwift
+
+class i2020_03_10_ReSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
